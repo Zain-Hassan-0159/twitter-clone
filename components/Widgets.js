9d624ccf039167851b47f07c2576011ee15773a3
Zain Hassan <zainhassan927@gmail.com>
2022-09-02 03:55:49 +0500

    Who to follow section completed

--- components/Widgets.js
+++ components/Widgets.js
@@ -2,11 +2,12 @@ import { SearchIcon } from "@heroicons/react/outline";
 import { useState } from "react";
 import News from "./News";
 
-export default function Widgets({newsResults}) {
+export default function Widgets({newsResults, randomUsersResults}) {
   const [ArticleNum, setArticleNum] = useState(3);
+  const [UserNumRandom, setUserNumRandom] = useState(3);
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
-      {/* Search Bar */}
+        {/* Search Bar */}
         <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
             <div className="flex items-center p-3 rounded-full relative">
                 <SearchIcon className="h-5 z-50 text-gray-500" />
@@ -23,6 +24,22 @@ export default function Widgets({newsResults}) {
           )
         )}
         <button onClick={()=> setArticleNum(ArticleNum +3) } className="text-blue-300 pl-4 pb-3 hover:text-blue-400">Show More</button>
+        </div>
+
+        {/* Who to follow */}
+        <div className="sticky top-16 text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[96%] xl:w-[75%] ">
+        <h4 className="font-bold text-xl px-4">Who to follow</h4>
+        {randomUsersResults.slice(0, UserNumRandom).map(randomUser => (
+          <div className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200" key={randomUser.login.username} >
+            <img className="rounded-full" width="40" src={randomUser.picture.thumbnail} />
+            <div className="truncate ml-4 leading-5">
+              <h4 className="font-bold hover:underline text-[14px] truncate">{randomUser.login.username}</h4>
+              <h5 className="text-[13px] text-gray-500 truncate">{randomUser.name.first +" "+ randomUser.name.last}</h5>
+            </div>
+            <button className="ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold" type="">Follow</button>
+          </div>
+        ))}
+        <button onClick={()=> setUserNumRandom(UserNumRandom +3) } className="text-blue-300 pl-4 pb-3 hover:text-blue-400">Show More</button>
         </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
 
-export default function Home({newsResults}) {
+export default function Home({newsResults, randomUsersResults}) {
   return (
     <div >
       <Head>
@@ -20,7 +20,7 @@ export default function Home({newsResults}) {
         <Feed />
 
         {/* Widgets */}
-        <Widgets newsResults = {newsResults.articles} />
+        <Widgets newsResults = {newsResults.articles} randomUsersResults={randomUsersResults.results} />
 
         {/* Modal */}
       </main>
@@ -35,9 +35,15 @@ export default function Home({newsResults}) {
 
 export async function getServerSideProps(){
   const newsResults = await fetch("https://saurav.tech/NewsAPI/top-headlines/category/health/us.json").then(res => res.json());
+
+  // Who to follow
+
+  const randomUsersResults = await fetch("https://randomuser.me/api/?results=50&inc=name,login,picture").then(res => res.json());
+
   return{
     props: {
-      newsResults
+      newsResults,
+      randomUsersResults
     }
   }
 }
