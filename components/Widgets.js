0f383bbcbfcebcb793eb157426e02c7efe4aa5b7
Zain Hassan <zainhassan927@gmail.com>
2022-09-01 05:01:26 +0500

    news section and api

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,6 +1,9 @@
 import { SearchIcon } from "@heroicons/react/outline";
+import { useState } from "react";
+import News from "./News";
 
-export default function Widgets() {
+export default function Widgets({newsResults}) {
+  const [ArticleNum, setArticleNum] = useState(3);
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
       {/* Search Bar */}
@@ -9,6 +12,17 @@ export default function Widgets() {
                 <SearchIcon className="h-5 z-50 text-gray-500" />
                 <input className="absolute inset-0 rounded-full pl-11 border-gray-500 text-gray-700 focus:shadow-lg focus:bg-white bg-gray-100" type="text" placeholder="Search Twitter" />
             </div>
+        </div>
+
+        {/* NewsApi */}
+        <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[96%] xl:w-[75%] ">
+          <h4 className="font-bold text-xl px-4">What's happening</h4>
+          {newsResults.slice(0, ArticleNum).map(
+          (article) => (
+            <News key={article.title} article = {article} />
+          )
+        )}
+        <button onClick={()=> setArticleNum(ArticleNum +3) } className="text-blue-300 pl-4 pb-3 hover:text-blue-400">Show More</button>
         </div>
     </div>
   )
