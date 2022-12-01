8a2a9ec74ce4e0ff851807eea815e100117bea48
Zain Hassan <zainhassan927@gmail.com>
2022-12-02 00:42:12 +0500

    fetch the posts from firestore and show them and install the package for timestamp

--- components/Feed.js
+++ components/Feed.js
@@ -1,38 +1,16 @@
 import {SparklesIcon} from "@heroicons/react/outline"
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
 import Input from "./Input"
 import Post from "./Post";
 
 export default function Feed() {
+  const [posts, setPosts] = useState([]);
 
-  const posts = [
-    {
-      id: "1",
-      name: "Zain Hassan",
-      username: "zainhassanPak",
-      userImage: "https://hassanzain.com/wp-content/uploads/2021/10/zainhassan.jpg",
-      img: "https://images.unsplash.com/photo-1555601568-c9e6f328489b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-      text: "This is my first post!",
-      timestamp: "2 hours ago"
-    },
-    {
-      id: "2",
-      name: "Zain Hassan",
-      username: "zainhassanPak",
-      userImage: "https://hassanzain.com/wp-content/uploads/2021/10/zainhassan.jpg",
-      img: "https://images.unsplash.com/photo-1455390582262-044cdead277a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80",
-      text: "This is my second post!",
-      timestamp: "1 hours ago"
-    },
-    {
-      id: "3",
-      name: "Zain Hassan",
-      username: "zainhassanPak",
-      userImage: "https://hassanzain.com/wp-content/uploads/2021/10/zainhassan.jpg",
-      img: "https://images.unsplash.com/photo-1447069387593-a5de0862481e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80",
-      text: "This is my third post!",
-      timestamp: "6 mins ago"
-    },
-  ];
+  useEffect(()=>{
+    return onSnapshot(query(collection(db, "posts"), orderBy("timestamp", "desc")), (snapshot) => {setPosts(snapshot.docs)})
+  }, [])
 
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
